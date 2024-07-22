f39d960faa5090e9177bbf6ee15a75831959107f
Hsu Myat Yadanar Oo <hsumyatyadanaroo1@gmail.com>
2024-07-23 06:39:16 +0800

    Upadting files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
+function Car(props){
+    return <h1>I am a {props.color} car</h1>
+}
+function Garage(){
+    return(
+        <>
+        <h1>Who lives in my garage</h1>
+        <Car/>
+        </>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('sandy'));
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render()
+root.render(<Garage/>)
